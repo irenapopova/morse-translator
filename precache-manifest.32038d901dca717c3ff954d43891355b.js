1410f33e5d0a947cc635a60f1ece30dd2c2dd96b
Irena <irenejpopova6@gmail.com>
2020-06-18 10:21:39 +0200

    Updates

---
+++ precache-manifest.32038d901dca717c3ff954d43891355b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e20e79ab8ff7d6315ff",
+    "url": "/static/css/main.5c9aa9a4.chunk.css"
+  },
+  {
+    "revision": "1e20e79ab8ff7d6315ff",
+    "url": "/static/js/main.1e20e79a.chunk.js"
+  },
+  {
+    "revision": "feafdfd87a60823877a5",
+    "url": "/static/js/1.feafdfd8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "53bb82a0ac932de5b0b3939178db4424",
+    "url": "/index.html"
+  }
+];
